0474ac96e573f3b597bf5d442621146fbf7959ab
aboyles <aboyles05@gmail.com>
2022-03-21 15:58:01 -0400

    adding nav bar/email form logic

--- server.py
+++ server.py
@@ -12,5 +12,43 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/landing')
+def landing():
+    return render_template('landing.html')
+
+
+@app.route('/contact', methods=["POST"])
+def contact():
+    from_email = request.form["email"]
+    email_message = request.form["message"]
+    name = request.form["name"]
+    send_mail(from_email, email_message, name)
+
+
+def send_mail(email, message, name):
+    smtp_email = os.getenv("my_email")
+    smtp_pass = os.getenv("password")
+
+    if email == "" or name == "" or message == "":
+        return render_template("email_failure.html", email=email, name=name, message=message)
+    else:
+        try:
+            with SMTP("smtp.gmail.com", 587) as connection:
+                connection.starttls()
+                connection.login(user=smtp_email, password=smtp_pass)
+                connection.sendmail(
+                    from_addr=email,
+                    to_addrs="codejetsmtp@gmail.com",
+                    msg=f"Subject: New Inquiry from {name} at {email}!\n\n"                       
+                        f"{message}")
+        except SMTPResponseException as e:
+            error_message = f"Sorry we could not complete the request due to{e.smtp_error}"
+            return error_message
+        else:
+            message = "We will be in touch with you soon, " \
+                      "please allow 3 business days for us to respond"
+            return render_template("email_success.html", result=message)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
