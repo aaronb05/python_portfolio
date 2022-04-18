b90b5323154e6dbb9c502c1cb49d370c648d5823
aaronb05 <aboyles05@gmail.com>
2022-04-18 11:29:34 -0400

    updated banner and contact form

--- server.py
+++ server.py
@@ -12,9 +12,9 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/landing')
+@app.route('/about')
 def landing():
-    return render_template('landing.html')
+    return render_template('about.html')
 
 
 @app.route('/contact', methods=["POST"])
@@ -26,7 +26,7 @@ def contact():
 
 
 def send_mail(email, message, name):
-    smtp_email = os.getenv("my_email")
+    smtp_email = os.getenv("email")
     smtp_pass = os.getenv("password")
 
     if email == "" or name == "" or message == "":
@@ -43,11 +43,11 @@ def send_mail(email, message, name):
                         f"{message}")
         except SMTPResponseException as e:
             error_message = f"Sorry we could not complete the request due to{e.smtp_error}"
-            return error_message
+            return render_template("email_failure", result=error_message)
         else:
             message = "We will be in touch with you soon, " \
                       "please allow 3 business days for us to respond"
-            return render_template("email_success.html", result=message)
+            return render_template("email_result.html", result=message)
 
 
 if __name__ == "__main__":
